09ea1db7df95044a2fbae1269ce2bf5d703bbc4d
Jon Buckley <jonbuckley33@gmail.com>
2021-07-02 23:33:39 -0700

    Refresh arrival times and clear display on exit

--- rgbdisplay.py
+++ rgbdisplay.py
@@ -1,5 +1,6 @@
 from rgbmatrix import graphics
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
+import threading
 
 DISPLAY_ROWS=32
 DISPLAY_COLUMNS=64
@@ -9,6 +10,20 @@ FIRST_LINE_COLOR=graphics.Color(255, 0, 0)
 SECOND_LINE_COLOR=graphics.Color(0, 255, 255)
 FIRST_LINE_Y_POS=10
 SECOND_LINE_Y_POS=25
+TICKS_BEFORE_SCROLLING = 30
+
+def _text_for_arrival(direction, arrival):
+  if arrival is None:
+    return "No %s train scheduled :(" % direction
+  return "Next train %s: %s" % (direction, arrival.eta.strftime("%X"))
+
+TICKS_PER_STATE = {
+  "BEGINNING_OF_LINE": 90,
+  # End of scrolling state is determined by length of text,
+  # not number of ticks.
+  "SCROLLING": -1,
+  "END_OF_LINE": 90,
+}
 
 class RgbDisplay:
   def __init__(self, *args, **kwargs):
@@ -16,28 +31,54 @@ class RgbDisplay:
     options.rows = DISPLAY_ROWS
     options.cols = DISPLAY_COLUMNS
     options.gpio_slowdown = LED_SLOWDOWN_GPIO
+
+    self.lock = threading.Lock()
     self.matrix = RGBMatrix(options = options)
     self.font = graphics.Font()
     self.font.LoadFont(FONT_FILE)
     self.canvas = self.matrix.CreateFrameCanvas()
-    self.position = self.canvas.width
+    self.position = 0
+    self._transition_to_state("BEGINNING_OF_LINE")
+    self.text_length = 0
 
     # Display renders two lines of text, one on top
     # of the other.
     self.first_line = ""
     self.second_line = ""
 
-  def set_text(self, first_line, second_line):
-    self.first_line = first_line
-    self.second_line = second_line
+  def set_next_arrivals(self, northbound, southbound):
+    with self.lock:
+      self.first_line = _text_for_arrival("north", northbound)
+      self.second_line = _text_for_arrival("south", southbound)
+
+  def _transition_to_state(self, state):
+    self.state = state
+    self.ticks_until_next_state = TICKS_PER_STATE[self.state]
+
+  def _tick(self):
+    if self.state == "BEGINNING_OF_LINE":
+      if self.ticks_until_next_state <= 0:
+        self._transition_to_state("SCROLLING")
+    elif self.state == "SCROLLING":
+      self.position -= 1
+      if self.position + self.text_length < self.canvas.width:
+        self._transition_to_state("END_OF_LINE")
+    elif self.state == "END_OF_LINE":
+      if self.ticks_until_next_state <= 0:
+        self.position = 0
+        self._transition_to_state("BEGINNING_OF_LINE")
+    self.ticks_until_next_state -= 1
 
   def update(self):
-    self.canvas.Clear()
+    with self.lock:
+      self._tick()
 
-    top_len = graphics.DrawText(self.canvas, self.font, self.position, FIRST_LINE_Y_POS, FIRST_LINE_COLOR, self.first_line)
-    bottom_len = graphics.DrawText(self.canvas, self.font, self.position, SECOND_LINE_Y_POS, SECOND_LINE_COLOR, self.second_line)
-    self.position -= 1
-    if self.position + max(top_len, bottom_len) < 0:
-      self.position = self.canvas.width
+      self.canvas.Clear()
+      top_len = graphics.DrawText(self.canvas, self.font, self.position, FIRST_LINE_Y_POS, FIRST_LINE_COLOR, self.first_line)
+      bottom_len = graphics.DrawText(self.canvas, self.font, self.position, SECOND_LINE_Y_POS, SECOND_LINE_COLOR, self.second_line)
+      self.text_length = max(top_len, bottom_len)
+      self.matrix.SwapOnVSync(self.canvas)
 
-    self.matrix.SwapOnVSync(self.canvas)
+  def clear(self):
+    with self.lock:
+      self.canvas.Clear()
---
+++ track.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import onebusaway
+import logging
+from rgbdisplay import RgbDisplay
+import threading
+import time
+
+COLUMBIA_CITY_NORTHBOUND_STOP_ID='1_55778'
+COLUMBIA_CITY_SOUTHBOUND_STOP_ID='1_56039'
+PAINT_FPS = 30
+FETCH_FPS = 0.05  # 1 fetch per 20 seconds
+
+def run():
+  display = RgbDisplay() 
+  try:   
+    paint_thread = threading.Thread(target=paint, args=(display,), daemon=True)
+    update_arrivals_thread = threading.Thread(target=update_arrivals, args=(display,), daemon=True)
+    
+    paint_thread.start()
+    update_arrivals_thread.start()
+
+    while True:
+      time.sleep(1.0)
+  finally:
+    display.clear()
+
+def paint(display):
+  while True:
+    display.update()
+    time.sleep(1.0 / PAINT_FPS)
+
+def update_arrivals(display):
+  while True:
+    logging.info("fetching arrivals...")
+    try:
+      northbound_arrivals = onebusaway.fetch_arrivals(COLUMBIA_CITY_NORTHBOUND_STOP_ID)
+      southbound_arrivals = onebusaway.fetch_arrivals(COLUMBIA_CITY_SOUTHBOUND_STOP_ID)
+
+      logging.info("found %s northbound and %s southbound arrivals" % (len(northbound_arrivals), len(southbound_arrivals)))
+
+      next_northbound_arrival = northbound_arrivals[0] if len(northbound_arrivals) > 0 else None
+      next_southbound_arrival = southbound_arrivals[0] if len(southbound_arrivals) > 0 else None
+      display.set_next_arrivals(next_northbound_arrival, next_southbound_arrival)
+    except Exception:
+      logging.exception("failed to fetch arrivals")
+    finally:
+      time.sleep(1.0 / FETCH_FPS)
+
+
+if __name__ == "__main__":
+  logging.basicConfig(level=logging.NOTSET)
+  run()
